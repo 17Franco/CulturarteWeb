2aeb935dfe11f1179c37ef6e50c847623d1ce829
Martindevelopf <martinfer820@gmail.com>
2025-10-02 22:41:20 -0300

    lista de categorias desde el backend

---
+++ src/main/java/Servlets/Categorias.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import logica.Fabrica;
+import logica.IController;
+
+@MultipartConfig
+@WebServlet(name="Categorias", urlPatterns={"/Categorias"})
+public class Categorias extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        IController controller= Fabrica.getInstance().getController();
+        // TODO: listaCategiras tiene que devolver un mapa de categorias 
+        // con la lista de subcategorias
+        List<String> categorias = controller.ListaCategoria();
+        request.setAttribute("categorias", categorias);
+      //  request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
